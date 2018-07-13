9852c3e170adc2d81a70701f346daf0853ca673d
Allen <aaboytes@ucsc.edu>
2018-07-13 00:39:52 -0700

    initialized repository, tracking userspace block finder

---
+++ ffblock.c
@@ -0,0 +1,21 @@
+/* 
+ * File: ffblock.c
+ * Author: Allen Aboytes
+ * Date: 7/12/18
+ *
+ * Desc: program written in user space finds
+ * 		 free space block in a block device.
+ */
+
+/* brain storm main functions needed to perform task */
+/*
+	flags for modularity (getopt)
+		-p path to device
+		-f filesystem type
+	open()
+	stat() --> for a block device
+		look specifically at vfat
+
+*/
+
+/* include directives */
