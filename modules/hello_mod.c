171172eb6cb2caf50818d4bef81bf7592184daa5
Allen <aaboytes@ucsc.edu>
2018-07-25 19:55:56 -0700

    organized module directory

---
+++ modules/hello_mod.c
@@ -0,0 +1,16 @@
+/* kernel projects for linux
+ * garry nutt
+ * hello world module
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int init_module()
+{
+	return;
+}
+
+void cleanup_module()
+{
+
+}
