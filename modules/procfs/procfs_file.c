7ee1ceecfa4c30edc95794b1ccfe9530f775901e
Allen <aaboytes@ucsc.edu>
2018-07-30 23:35:30 -0700

    reorganize repo

---
+++ modules/procfs/procfs_file.c
@@ -0,0 +1,39 @@
+/* 
+ * Allen Aboytes 
+ * 7/26/2018
+ *
+ * procfs_file.c - creates a file in /proc
+ *
+ */
+ 
+ /* standard headers for kernel modules */
+ #include <linux/init.h>
+ #include <linux/module.h>
+ #include <linux/kernel.h>
+ 
+ /* 
+  * hello_init() - called when module is loaded
+  * RETURN: 0 if successfully loaded, nonzero else
+  */
+static int hello_init(void)
+{
+	printk(KERN_ALERT "I'm a man on the inside.\n");
+	return 0;
+}
+
+/* 
+ * hello_exit() - called when module is removed
+ */
+ static void hello_exit(void)
+ {
+	 printk(KERN_ALERT "Now time for my escape.\n");
+ }
+ 
+ /* register module start and end functions */
+ module_init(hello_init);
+ module_exit(hello_exit);
+ 
+ /* basic description of module */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Zed");
+MODULE_DESCRIPTION("A hello world module");
