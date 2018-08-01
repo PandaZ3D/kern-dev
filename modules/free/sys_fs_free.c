1fceae89f44e06834d6ff0e1238cb65e4eff77bb
Allen Aboytes <aaboytes@ucsc.edu>
2018-08-01 13:03:10 -0700

    changed to make directory under /sys/fs/free with name of block device

--- modules/free/sys_fs_free.c
+++ modules/free/sys_fs_free.c
@@ -2,7 +2,8 @@
  * Allen Aboytes 
  * 8/1/2018
  *
- * sysfs_file.c - creates a file under /sys/fs/free
+ * sys_fs_free.c - creates a file under /sys/fs/free
+ * for a specific block device
  *
  */
  
@@ -13,85 +14,80 @@
 #include <linux/sysfs.h>
 #include <linux/fs.h> /* fs_kobj */
 
-#define NAME 		"newfile"	/* file name */
-#define PERM 		0444 		/* file permissions */
-#define PARENT 		NULL
+#define NAME 		"free"	/* directory name */
+#define PARENT 		fs_kobj
 
 #define FILE_ATTR_RO(_name)\
 	struct kobj_attribute kobj_attr_##_name = __ATTR_RO(_name)
 
-static struct kobject* new_kobj;
-static int special = 0xfeed;
-
+static struct kobject *free_kobj, *bd_kobj;
+static int special = 0x80085;
+static const char* name = "sda";
 /*
  * sysfs allocates a buffer of size (PAGE_SIZE)
  * On read(2) method should fill the entire buffer.
  * return the number of bytes, return value of scnprintf()
  */
 static ssize_t 
-newfile_show(struct kobject *kobj, struct kobj_attribute *attr, 
+free_show(struct kobject *kobj, struct kobj_attribute *attr, 
 	char *buf)
 {
 	return scnprintf(buf, PAGE_SIZE, "0x%X\n", special);
 }
 
-/*
- * returns the number of bytes used from the buffer. If the
- * entire buffer has been used, just return the count argument.
- */
-/*
-static ssize_t
-newfile_store(struct kobject *kobj, struct kobj_attribute *attr,
-	const char *buf, size_t count)
-{
-	return 0;
-}
-*/
-
 /* kobject attributes for file */
-static FILE_ATTR_RO(newfile);
+static FILE_ATTR_RO(free);
 
 /* 
  * RETURN: 0 if successfully loaded, nonzero else
  */
-static int newfile_init(void)
+static int free_init(void)
 {
 	int ret = 0;
 	
 	/* create new kobject for use in sysfs (a new directory) */
-	//new_kobj = kobject_create_and_add("newfolder", PARENT);
+	free_kobj = kobject_create_and_add(NAME, PARENT);
 	
 	/* not enough memory to allocate */
-	//if(new_kobj == NULL)
-	//{
-	//	printk(KERN_ALERT "Error: on creating sysfs file entry.\n");
-	//	return -ENOMEM;
-	//}
+	if(free_kobj == NULL)
+	{
+		printk(KERN_ALERT "Error: on creating sysfs directory entry.\n");
+		return -ENOMEM;
+	}
+
+	/* create new kobject under /sys/free for block device */
+	bd_kobj = kobject_create_and_add(name, free_kobj);
+	
+	/* not enough memory to allocate */
+	if(bd_kobj == NULL)
+	{
+		printk(KERN_ALERT "Error: on creating sysfs directory entry.\n");
+		return -ENOMEM;
+	}	
 
 	/* try to create new sysfs entry (file) */
-	//ret = sysfs_create_file(fs_kobj, &kobj_attr_newfile.attr);
+	ret = sysfs_create_file(bd_kobj, &kobj_attr_free.attr);
 
 	/* log error message */
-	//if(ret)
-		//printk(KERN_ALERT "Error: on creating sysfs file entry.\n");
-	sysfs_remove_file(fs_kobj, &kobj_attr_newfile.attr);
+	if(ret)
+		printk(KERN_ALERT "Error: on creating sysfs file entry.\n");
 	
-	return 0;
+	return ret;
 }
 
 /* 
  * 
  */
-static void newfile_exit(void)
+static void free_exit(void)
 {
 	/* remove instance of kernel object */
-	//kobject_put(new_kobj);
+	kobject_put(free_kobj);
 	//sysfs_remove_file(fs_kobj, &kobj_attr_newfile.attr);
 }
  
 /* register module start and end functions */
-module_init(newfile_init);
-module_exit(newfile_exit);
+module_init(free_init);
+module_exit(free_exit);
  
 /* basic description of module */
 MODULE_LICENSE("GPL");
