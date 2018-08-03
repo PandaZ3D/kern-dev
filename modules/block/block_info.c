35c06103dd07a10cf9cb36fcf4fd89f4d19b9b92
Allen Aboytes <aaboytes@ucsc.edu>
2018-08-03 00:32:27 -0700

    modeule gets reference to blkdev and prints basic info

--- modules/block/block_info.c
+++ modules/block/block_info.c
@@ -4,7 +4,7 @@
  *
  * block_info.c - prints device info
  *
- * Module taints kernel on build
+ * Externel module taints kernel
  */
  
 /* standard headers for kernel modules */
@@ -12,37 +12,63 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+/* file system includes */
+#include <linux/fs.h>
+#include <linux/genhd.h>
+
 #define PERM	0000 /* parameter permissions */
-#define MAX		64	 /* max number of arguments */
+#define MODE 	FMODE_READ
 
 /* message passed in from user */
 static char* dev;
+static struct block_device* bdev;
 
 /* set string to msg pointer */
 module_param(dev, charp, PERM);
 MODULE_PARM_DESC(dev, "Device path name");
 
- /* 
-  * hello_init() - called when module is loaded
-  * RETURN: 0 if successfully loaded, nonzero else
-  */
-static int hello_init(void)
+static int blk_init(void)
 {
-	printk(KERN_INFO "Device to be opened: %s\n", dev);
+	struct hd_struct* part;
+
+	bdev = blkdev_get_by_path(dev, MODE, NULL);
+
+	if(IS_ERR_OR_NULL(bdev))
+		return PTR_ERR(bdev);
+
+	/* successful opening */
+	//printk(KERN_INFO "Success: opened: %s\n", dev);
+
+	/* we are working with sdb */
+	part = bdev->bd_part;
+
+	if(part == NULL)
+	{
+		printk(KERN_ALERT "Unable to obtain partition.");
+		blkdev_put(bdev, MODE);
+	}
+
+	printk(KERN_INFO "Device name:%s Disk name:%s, Maj:%d, Min:%d", 
+	dev_name(&part->__dev),
+	bdev->bd_disk->disk_name,
+	bdev->bd_disk->major,
+	part->partno);
+
 	return 0;
 }
 
-/* 
- * hello_exit() - called when module is removed
+/*  
+ * blkdev_exit() - called when module is removed
  */
-static void hello_exit(void)
+static void blk_exit(void)
 {
 	printk(KERN_INFO "Graceful Exit.\n");
+	blkdev_put(bdev, MODE);
 }
  
 /* register module start and end functions */
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(blk_init);
+module_exit(blk_exit);
  
 /* basic description of module */
 MODULE_LICENSE("GPL");
