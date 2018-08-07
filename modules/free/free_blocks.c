48edf88153becc10f8418b0e42ce6d011bec9a96
Allen Aboytes <aaboytes@ucsc.edu>
2018-08-07 01:14:21 -0700

    added kobj get function

--- modules/free/free_blocks.c
+++ modules/free/free_blocks.c
@@ -14,15 +14,22 @@
 #include <linux/sysfs.h>
 #include <linux/fs.h> /* fs_kobj */
 
+#include "trivial.h"
+
 #define NAME 		"free"	/* directory name */
 #define PARENT 		fs_kobj
+#define MODE 	FMODE_READ
 
 #define FILE_ATTR_RO(_name)\
 	struct kobj_attribute kobj_attr_##_name = __ATTR_RO(_name)
 
+/* block device reference */
+static struct block_device* bdev;
+
 static struct kobject *free_kobj, *bd_kobj;
 static int special = 0x80085;
-static const char* name = "sda";
+static char* path = "/dev/sdb1";
+
 /*
  * sysfs allocates a buffer of size (PAGE_SIZE)
  * On read(2) method should fill the entire buffer.
@@ -55,15 +62,17 @@ static int free_init(void)
 		return -ENOMEM;
 	}
 
-	/* create new kobject under /sys/free for block device */
-	bd_kobj = kobject_create_and_add(name, free_kobj);
-	
-	/* not enough memory to allocate */
-	if(bd_kobj == NULL)
+	/* obtain block device reference */
+	bdev = blkdev_get_by_path(path, MODE, NULL);
+
+	if(IS_ERR_OR_NULL(bdev))
 	{
-		printk(KERN_ALERT "Error: on creating sysfs directory entry.\n");
-		return -ENOMEM;
-	}	
+		ret = PTR_ERR(bdev);
+		goto fail;
+	}
+
+	/* create new kobject under /sys/free for block device */
+	bd_kobj = bdev_to_kobj(bdev);
 
 	/* try to create new sysfs entry (file) */
 	ret = sysfs_create_file(bd_kobj, &kobj_attr_free.attr);
@@ -71,10 +80,20 @@ static int free_init(void)
 	/* log error message */
 	if(ret)
 	{	
-		kobject_put(free_kobj);
-		kobject_put(bd_kobj);
 		printk(KERN_ALERT "Error: on creating sysfs file entry.\n");
+		goto fail;
 	}
+
+	return 0;
+
+fail: 
+
+	if(free_kobj)
+		kobject_put(free_kobj);
+	if(bd_kobj)
+		kobject_put(bd_kobj);
+	if(bdev)
+		blkdev_put(bdev, MODE);
 
 	return ret;
 }
@@ -86,6 +105,8 @@ static void free_exit(void)
 {
 	/* remove instance of kernel object */
 	kobject_put(free_kobj);
+	kobject_put(bd_kobj);
+	blkdev_put(bdev, MODE);
 	//sysfs_remove_file(fs_kobj, &kobj_attr_newfile.attr);
 }
  
---
+++ modules/free/trivial.h
@@ -0,0 +1,59 @@
+/* A collection of a trivial
+ * set of functions implemented
+ * due to non-exported symbols or
+ * for kernel lack-of-need issues.
+ */
+#ifndef __TRIVIAL_H__
+#define __TRIVIAL_H__
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/genhd.h>
+
+/* type modifiers */
+#define bdev_to_part(bdev) 	(bdev->bd_part)
+#define dev_to_kobj(dev)	(&((dev)->kobj))
+
+#define get_fs_name(fs) 	(fs->name)
+
+/* implementation: /fs/filsystems.c */
+static inline struct file_system_type*
+get_file_system(struct file_system_type* fs)
+{
+	__module_get(fs->owner);
+	return fs;
+}
+
+/* implementation: /fs/filsystems.c */
+static inline void
+put_file_system(struct file_system_type* fs)
+{
+	module_put(fs->owner);
+}
+
+/* returns kobj or NULL */
+static inline struct kobject*
+bdev_to_kobj(struct block_device* bdev)
+{
+	struct kobject* kobj = NULL;
+	struct device* dev;
+	struct hd_struct* part;
+
+	part = bdev_to_part(bdev);
+	if(part == NULL)
+		goto out;
+	dev = part_to_dev(part);
+	if(dev == NULL)
+		goto out;
+	kobj = dev_to_kobj(dev);
+	kobject_get(kobj);
+
+out:
+	
+	part = NULL;
+	dev = NULL;
+
+	return kobj;
+}
+
+#endif /* __TRIVIAL_H__ */
